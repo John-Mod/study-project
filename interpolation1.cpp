7719e289ae305eb095c3ca5b24ed0a1fd9edecd1
John-Mod <victoria8524@gmail.com>
2020-04-23 10:29:39 +0700

    Added interpolation1.cpp

---
+++ interpolation1.cpp
@@ -0,0 +1,12 @@
+
+#include "interpolation.h"
+
+float lagrange(const std::vector<float>& x, const std::vector<float>& y,
+	float arg)
+{
+	assert(x.size() == y.size());
+
+	float polynomial = 0;
+	// ...
+	return polynomial;
+}
